bbcb574e0a366f9c1f047e5e7b7c17f223ea0ce1
thairyo <sonthai1310.works@gmail.com>
2022-07-08 14:30:56 +0700

    Initial sample data.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,19 @@
-import Task from "components/Task/Task";
 import React from "react";
+import Card from "components/Card/Card";
+import { mapOrder } from "utilities/sorts";
 import "./Column.scss";
 
-const Column = () => {
+const Column = (props) => {
+  const { column } = props;
+  const cards = mapOrder(column.cards, column.cardOrder, "id");
+
   return (
     <div className="column">
-      <header>Brainstorm</header>
-      <ul className="task-list">
-        <Task/>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
-        <li className="task-item">Add what you'd like to work on below</li>
+      <header>{column.title}</header>
+      <ul className="card-list">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </ul>
       <footer>Add another card</footer>
     </div>
